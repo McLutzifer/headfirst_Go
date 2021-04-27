5a658db0d0bed4013d4596bec661acd9abd27e24
McLutzifer <lukasschumi@gmx.at>
2021-04-27 14:48:43 +0200

    add math and string

--- first.go
+++ first.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, Go!")
 	fmt.Println("First argument", "Second argument")
 	fmt.Println("Another argument")
+	fmt.Println(strings.Title("head first go"))
+	fmt.Println(math.Floor(2.75))
 }
