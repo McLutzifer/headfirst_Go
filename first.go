bfeec55b299c4b64ee5f36723fe3ae49a3c3a58b
McLutzifer <lukasschumi@gmx.at>
2021-04-27 14:51:35 +0200

    add TypeOf

--- first.go
+++ first.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
@@ -12,4 +13,11 @@ func main() {
 	fmt.Println("Another argument")
 	fmt.Println(strings.Title("head first go"))
 	fmt.Println(math.Floor(2.75))
+	fmt.Println(7 / 5.0)
+
+	fmt.Println(reflect.TypeOf(42))
+	fmt.Println(reflect.TypeOf(3.1415))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf("Hello, Go"))
+	fmt.Println(reflect.TypeOf('A'))
 }
