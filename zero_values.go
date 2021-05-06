bbd521245e7cdfe38b7d9d7e55a88ef83172db11
McLutzifer <lukasschumi@gmx.at>
2021-05-06 19:20:40 +0200

    add bool

--- zero_values.go
+++ zero_values.go
@@ -6,8 +6,10 @@ func main() {
 	var myInt int
 	var myFloat float64
 	var myString string
+	var myBool bool
 
 	fmt.Println(myInt)
 	fmt.Println(myFloat)
 	fmt.Println(myString)
+	fmt.Println(myBool)
 }
