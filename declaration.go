2daebb842e24d93000398ef42f96620956fbc3fd
McLutzifer <lukasschumi@gmx.at>
2021-05-06 19:14:34 +0200

    update

--- declaration.go
+++ declaration.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	var quantity int
 	var length, width float64
 	var customerName string
 
-	quantity = 2
+	quantity = 4
 	customerName = "Damon Cole"
+	length, width = 1.2, 2.4
+
+	fmt.Println(customerName)
+	fmt.Println("has ordered", quantity, "sheets")
 }
